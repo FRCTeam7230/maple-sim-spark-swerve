8cbf78fccbeb85b55a38cba9baa9c7b30ad54765
ktang <ktang21@outlook.com>
2025-01-28 21:18:00 -0800

    Initial checkin.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # maple-sim-spark-swerve
-Simulation of real physics using maple-sim.
+Simulation of real physics using maple-sim. Code is based on [AdvantageKit SparkMax Swerve Template](https://github.com/Shenzhen-Robotics-Alliance/maple-sim/tree/main/templates/AdvantageKit_SparkSwerveTemplate-maple-sim).
---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,17 @@
+package frc.robot;
+
+/** Automatically generated file containing build version information. */
+public final class BuildConstants {
+    public static final String MAVEN_GROUP = "";
+    public static final String MAVEN_NAME = "AdvantageKit_SparkSwerveTemplate-maple-sim";
+    public static final String VERSION = "unspecified";
+    public static final int GIT_REVISION = 438;
+    public static final String GIT_SHA = "2746a0ab515965591cde201531791854f50bca56";
+    public static final String GIT_DATE = "2025-01-24 06:14:35 EST";
+    public static final String GIT_BRANCH = "main";
+    public static final String BUILD_DATE = "2025-01-26 18:59:47 EST";
+    public static final long BUILD_UNIX_TIME = 1737935987269L;
+    public static final int DIRTY = 0;
+
+    private BuildConstants() {}
+}
